59d9ac9d08dca73c617a80d23943f19f03738978
Lukiya Chen <lukiya@outlook.com>
2020-07-25 12:32:57 -0700

    update

--- src/OAuth2Net.Host/ClientExtensions.cs
+++ src/OAuth2Net.Host/ClientExtensions.cs
@@ -27,55 +27,7 @@ namespace Microsoft.Extensions.DependencyInjection
             })
                 .AddCookie(cookieOptions =>
                 {
-                    cookieOptions.Events.OnValidatePrincipal = async x =>
-                    {
-                        // since our cookie lifetime is based on the access token one,
-                        // check if we're more than halfway of the cookie lifetime
-                        var now = DateTimeOffset.UtcNow;
-                        var timeElapsed = now.Subtract(x.Properties.IssuedUtc.Value);
-                        var timeRemaining = x.Properties.ExpiresUtc.Value.Subtract(now);
-
-                        if (timeElapsed > timeRemaining)
-                        {
-                            var identity = (ClaimsIdentity)x.Principal.Identity;
-                            var accessTokenClaim = identity.FindFirst("access_token");
-                            var refreshTokenClaim = identity.FindFirst("refresh_token");
-
-                            // if we have to refresh, grab the refresh token from the claims, and request
-                            // new access token and refresh token
-                            var refreshToken = refreshTokenClaim.Value;
-                            var response = await new HttpClient().RequestRefreshTokenAsync(new RefreshTokenRequest
-                            {
-                                Address = options.TokenEndpoint,
-                                ClientId = options.ClientID,
-                                ClientSecret = options.ClientSecret,
-                                RefreshToken = refreshToken
-                            });
-
-                            if (!response.IsError)
-                            {
-                                // everything went right, remove old tokens and add new ones
-                                identity.RemoveClaim(accessTokenClaim);
-                                identity.RemoveClaim(refreshTokenClaim);
-
-                                identity.AddClaims(new[]
-                                {
-                                    new Claim("access_token", response.AccessToken),
-                                    new Claim("refresh_token", response.RefreshToken)
-                                });
-
-                                // indicate to the cookie middleware to renew the session cookie
-                                // the new lifetime will be the same as the old one, so the alignment
-                                // between cookie and access token is preserved
-                                x.ShouldRenew = true;
-                            }
-                            else
-                            {
-                                // refresh failed
-                                x.RejectPrincipal();
-                            }
-                        }
-                    };
+                    cookieOptions.Events.OnValidatePrincipal = x => ValidatePrincepal(x, options);
                 })
                 .AddOAuth(OAuthDefaults.DisplayName, oauthOptions =>
                 {
@@ -107,6 +59,56 @@ namespace Microsoft.Extensions.DependencyInjection
                 });
 
             return services;
+        }
+
+        private static async Task ValidatePrincepal(CookieValidatePrincipalContext context, OAuth2ClientOptions options)
+        {
+            // since our cookie lifetime is based on the access token one,
+            // check if we're more than halfway of the cookie lifetime
+            var now = DateTimeOffset.UtcNow;
+            var timeElapsed = now.Subtract(context.Properties.IssuedUtc.Value);
+            var timeRemaining = context.Properties.ExpiresUtc.Value.Subtract(now);
+
+            if (timeElapsed > timeRemaining)
+            {
+                var identity = (ClaimsIdentity)context.Principal.Identity;
+                var accessTokenClaim = identity.FindFirst("access_token");
+                var refreshTokenClaim = identity.FindFirst("refresh_token");
+
+                // if we have to refresh, grab the refresh token from the claims, and request
+                // new access token and refresh token
+                var refreshToken = refreshTokenClaim.Value;
+                var response = await new HttpClient().RequestRefreshTokenAsync(new RefreshTokenRequest
+                {
+                    Address = options.TokenEndpoint,
+                    ClientId = options.ClientID,
+                    ClientSecret = options.ClientSecret,
+                    RefreshToken = refreshToken
+                });
+
+                if (!response.IsError)
+                {
+                    // everything went right, remove old tokens and add new ones
+                    identity.RemoveClaim(accessTokenClaim);
+                    identity.RemoveClaim(refreshTokenClaim);
+
+                    identity.AddClaims(new[]
+                    {
+                                    new Claim("access_token", response.AccessToken),
+                                    new Claim("refresh_token", response.RefreshToken)
+                                });
+
+                    // indicate to the cookie middleware to renew the session cookie
+                    // the new lifetime will be the same as the old one, so the alignment
+                    // between cookie and access token is preserved
+                    context.ShouldRenew = true;
+                }
+                else
+                {
+                    // refresh failed
+                    context.RejectPrincipal();
+                }
+            }
         }
 
         private static void CheckOptions(OAuth2ClientOptions options)
