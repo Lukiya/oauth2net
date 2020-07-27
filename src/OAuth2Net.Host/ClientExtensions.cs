2097e0dbe64a549b5813a2bd1c972fc331769881
Lukiya Chen <lukiya@outlook.com>
2020-07-26 19:15:44 -0700

    update

--- src/OAuth2Net.Host/ClientExtensions.cs
+++ src/OAuth2Net.Host/ClientExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.IdentityModel.JsonWebTokens;
@@ -27,7 +28,7 @@ namespace Microsoft.Extensions.DependencyInjection
             })
                 .AddCookie(cookieOptions =>
                 {
-                    cookieOptions.Events.OnValidatePrincipal = x => ValidatePrincepal(x, options);
+                    cookieOptions.Events.OnValidatePrincipal = x => ValidatePrincipal(x, options);
                 })
                 .AddOAuth(OAuthDefaults.DisplayName, oauthOptions =>
                 {
@@ -61,46 +62,55 @@ namespace Microsoft.Extensions.DependencyInjection
             return services;
         }
 
-        private static async Task ValidatePrincepal(CookieValidatePrincipalContext context, OAuth2ClientOptions options)
+        private static async Task ValidatePrincipal(CookieValidatePrincipalContext context, OAuth2ClientOptions options)
         {
             // since our cookie lifetime is based on the access token one,
             // check if we're more than halfway of the cookie lifetime
-            var now = DateTimeOffset.UtcNow;
-            var timeElapsed = now.Subtract(context.Properties.IssuedUtc.Value);
-            var timeRemaining = context.Properties.ExpiresUtc.Value.Subtract(now);
+            //var now = DateTimeOffset.UtcNow;
+            //var timeElapsed = now.Subtract(context.Properties.IssuedUtc.Value);
+            //var timeRemaining = context.Properties.ExpiresUtc.Value.Subtract(now);
+            var expStr = context.Properties.GetTokenValue("expires_at");
+            var now = DateTime.Now;
+            var exp = DateTime.Parse(expStr);
 
-            if (timeElapsed > timeRemaining)
+            if (now > exp)
             {
-                var identity = (ClaimsIdentity)context.Principal.Identity;
-                var accessTokenClaim = identity.FindFirst("access_token");
-                var refreshTokenClaim = identity.FindFirst("refresh_token");
+                //var identity = (ClaimsIdentity)context.Principal.Identity;
+                //var accessTokenClaim = identity.FindFirst("access_token");
+                //var refreshTokenClaim = identity.FindFirst("refresh_token");
 
                 // if we have to refresh, grab the refresh token from the claims, and request
                 // new access token and refresh token
-                var refreshToken = refreshTokenClaim.Value;
+                var refreshToken = context.Properties.GetTokenValue(OAuth2Consts.Form_RefreshToken);
                 var response = await new HttpClient().RequestRefreshTokenAsync(new RefreshTokenRequest
                 {
                     Address = options.TokenEndpoint,
                     ClientId = options.ClientID,
                     ClientSecret = options.ClientSecret,
-                    RefreshToken = refreshToken
+                    RefreshToken = refreshToken,
+                    Scope = string.Join(OAuth2Consts.Seperator_Scope, options.Scopes)
                 });
 
                 if (!response.IsError)
                 {
-                    // everything went right, remove old tokens and add new ones
-                    identity.RemoveClaim(accessTokenClaim);
-                    identity.RemoveClaim(refreshTokenClaim);
+                    //// everything went right, remove old tokens and add new ones
+                    //identity.RemoveClaim(accessTokenClaim);
+                    //identity.RemoveClaim(refreshTokenClaim);
 
-                    identity.AddClaims(new[]
-                    {
-                                    new Claim("access_token", response.AccessToken),
-                                    new Claim("refresh_token", response.RefreshToken)
-                                });
+                    //identity.AddClaims(new[]
+                    //{
+                    //                new Claim("access_token", response.AccessToken),
+                    //                new Claim("refresh_token", response.RefreshToken)
+                    //            });
 
                     // indicate to the cookie middleware to renew the session cookie
                     // the new lifetime will be the same as the old one, so the alignment
                     // between cookie and access token is preserved
+
+                    context.Properties.UpdateTokenValue(OAuth2Consts.Form_AccessToken, response.AccessToken);
+                    context.Properties.UpdateTokenValue(OAuth2Consts.Form_RefreshToken, response.RefreshToken);
+                    //context.Properties.UpdateTokenValue(OAuth2Consts.Form_RefreshToken, response.ExpiresIn);
+
                     context.ShouldRenew = true;
                 }
                 else
