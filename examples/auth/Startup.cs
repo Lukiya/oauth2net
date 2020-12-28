724b687160d2d6149d451bebe3dc87e7e3b66ab1
Lukiya <lukiya@outlook.com>
2020-12-28 11:32:28 -0800

    update

--- examples/auth/Startup.cs
+++ examples/auth/Startup.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
