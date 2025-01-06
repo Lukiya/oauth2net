28ad1edccc2a7912cf121a0d00a4c177d8a1ede4
Lukiya Chen <lukiya@outlook.com>
2025-01-06 12:09:46 -0800

    fix https://dw.xxx.com:10032/signout -> https://[dw.xxx.com:10032]/signout

--- src/OAuth2NetCore.Host/DefaultClientServer.cs
+++ src/OAuth2NetCore.Host/DefaultClientServer.cs
@@ -12,8 +12,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace OAuth2NetCore.Host {
-    public class DefaultClientServer : IClientServer
-    {
+    public class DefaultClientServer : IClientServer {
         private readonly IStateStore _stateStore;
         private readonly IStateGenerator _stateGenerator;
         private readonly ITokenStore _tokenDTOStore;
@@ -31,8 +30,7 @@ namespace OAuth2NetCore.Host {
             , IHttpClientFactory httpClientFactory
             , ILogger<DefaultClientServer> logger
             , ClientOptions options
-        )
-        {
+        ) {
             _stateStore = stateStore;
             _stateGenerator = stateGenerator;
             _tokenDTOStore = tokenDTOStore;
@@ -48,8 +46,7 @@ namespace OAuth2NetCore.Host {
         /// <summary>
         /// handle sign out request
         /// </summary>
-        protected virtual async Task HandleSignOutRequestAsync(HttpContext context)
-        {
+        protected virtual async Task HandleSignOutRequestAsync(HttpContext context) {
             // save return url to state store
             var returnUrl = context.Request.Query[OAuth2Consts.Form_ReturnUrl].FirstOrDefault() ?? Uri.EscapeDataString("/");
             var state = await _stateGenerator.GenerateAsync();
@@ -57,10 +54,19 @@ namespace OAuth2NetCore.Host {
 
             // redirect to auth server
             var clientID = _options.ClientID;
-            var callbackUri = new UriBuilder();
-            callbackUri.Scheme = context.Request.Scheme;
-            callbackUri.Host = context.Request.Host.Value;
-            callbackUri.Path = _options.SignOutCallbackPath;
+            //var callbackUri = new UriBuilder();
+            //callbackUri.Scheme = context.Request.Scheme;
+            //callbackUri.Host = context.Request.Host.Value;
+            //callbackUri.Path = _options.SignOutCallbackPath;
+
+            // This change is to fix https://dw.xxx.com:10032/signout -> https://[dw.xxx.com:10032]/signout
+            var callbackUri = new UriBuilder {
+                Scheme = context.Request.Scheme,
+                Host = context.Request.Host.Host,
+                Port = context.Request.Host.Port ?? -1,
+                Path = _options.SignOutCallbackPath
+            };
+
             var targetUri = $"{_options.EndSessionEndpoint}?client_id={clientID}&redirect_uri={Uri.EscapeDataString(callbackUri.ToString())}&state={state}";
             context.Response.Redirect(targetUri);
         }
@@ -68,25 +74,21 @@ namespace OAuth2NetCore.Host {
         /// <summary>
         ///  handle sign out callback request
         /// </summary>
-        protected virtual async Task HandleSignOutCallbackRequestAsync(HttpContext context)
-        {
+        protected virtual async Task HandleSignOutCallbackRequestAsync(HttpContext context) {
             var state = context.Request.Query[OAuth2Consts.Form_State].FirstOrDefault();
 
             // read return url from store
             var returnUrl = await _stateStore.GetThenRemoveAsync(state);
-            if (!string.IsNullOrWhiteSpace(returnUrl))
-            {
+            if (!string.IsNullOrWhiteSpace(returnUrl)) {
                 var endSessionID = context.Request.Query[OAuth2Consts.Form_EndSessionID].FirstOrDefault();
-                if (string.IsNullOrWhiteSpace(endSessionID))
-                {
+                if (string.IsNullOrWhiteSpace(endSessionID)) {
                     context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
                     await context.Response.WriteAsync("missing es_id");
                     return;
                 }
 
                 var tokenDTO = await _tokenDTOStore.GetTokenDTOAsync();
-                if (tokenDTO != null)
-                {
+                if (tokenDTO != null) {
                     var httpClient = _httpClientFactory.CreateClient();
                     var resp = await httpClient.PostAsync(_options.EndSessionEndpoint, new FormUrlEncodedContent(new KeyValuePair<string, string>[] {
                         new KeyValuePair<string, string>(OAuth2Consts.Form_State, state),
@@ -95,8 +97,7 @@ namespace OAuth2NetCore.Host {
                         new KeyValuePair<string, string>(OAuth2Consts.Form_ClientSecret, _options.ClientSecret),
                         new KeyValuePair<string, string>(OAuth2Consts.Form_RefreshToken, tokenDTO.RefreshToken),
                     }));
-                    if (!resp.IsSuccessStatusCode)
-                    {
+                    if (!resp.IsSuccessStatusCode) {
                         var body = await resp.Content.ReadAsStringAsync();
                         _logger.LogWarning("Post end session request failed [{0}]:\n{1}", resp.StatusCode, body);
                     }
